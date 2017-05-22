f522bd5016ecbe1aed67080976d02cd6e814cfd2
SilentCC <17816853696@163.com>
2017-05-22 20:36:35 +0800

    FlaskProject

--- hello.py
+++ hello.py
@@ -1,18 +1,29 @@
 #!/bin/python
 
-from flask import Flask,make_response
+from flask import Flask,make_response,render_template
 from flask.ext.script import Manager
+
 app = Flask(__name__)
 
 manager = Manager(app)
 
 @app.route('/hello')
-def index():
+def hello():
     return '<h1>Hello World!</h1>'
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+"""
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello ,%s!</h1>' %name
+"""
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
 
 @app.route('/')
 def index2():
