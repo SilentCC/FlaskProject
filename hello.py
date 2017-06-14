62728c8160a1300959f693e83397d5c49204bb97
SilentCC <17816853696@163.com>
2017-06-14 17:26:52 +0800

    FlaskProject

--- hello.py
+++ hello.py
@@ -2,10 +2,12 @@
 
 from flask import Flask,make_response,render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/hello')
 def hello():
@@ -30,6 +32,14 @@ def index2():
         response=make_response('<h1>This document carries a cookie!</h1>')
         response.set_cookie('answer','42')
         return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
 
 if __name__ =='__main__':
     manager.run()
